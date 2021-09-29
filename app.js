5180bb41332a353ce3d2dbd112c9b3c3edbd0a59
srsahana <sahanasr24102000@gmail.com>
2021-09-29 19:47:46 +0530

    append child

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var heading = document.getElementById('heading')
-var body = document.querySelector('body')
+var list=document.querySelector('ul')
 
-console.log(heading)
-heading.innerHTML="gggg"
-heading.style="color:red;font-size:2rem"
+var arr=['go to the gym','cook ','eat']
 
-heading.classList.add('five')
-heading.classList.remove('one')
+arr.push('complete')
 
-
-body.classList.add('dark')
+for (var i=0;i<arr.length; i++){
+    var element=document.createElement('li')
+    var textNode= document.createTextNode(arr[i])
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
