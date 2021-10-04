2dbcf903c82c4e02127e4944df1f0eed4c93fb1d
srsahana <sahanasr24102000@gmail.com>
2021-10-04 19:11:32 +0530

    $ syntax

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log(this.alert('gg'))
+let manu='hhhhh'
+
+let car={
+    name:"c class",
+    manu:"mer",
+    print:function (a){
+        console.log(`${this.name} waaasssss ${this.manu}`)
+
+    }
+}
+car.print()
