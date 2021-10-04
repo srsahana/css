0935e0c25819667748ccd740db7482c18ed28615
srsahana <sahanasr24102000@gmail.com>
2021-10-04 19:47:01 +0530

    fetch

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-let manu='hhhhh'
-
-let car={
-    name:"c class",
-    manu:"mer",
-    print: ()=>{
-        console.log(`${this.name} waaasssss ${this.manu}`)
-
-    }
-}
-console.log(Object.keys(car))
-console.log(Object.values(car))
+var data=fetch('https://jsonplaceholder.typicode.com/todos/10')
+console.log(data)
