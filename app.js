757a1fd9fa7904193fdbfb7f96f83c861b59484d
srsahana <sahanasr24102000@gmail.com>
2021-10-05 19:15:29 +0530

    resolve reject promise

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-setTimeout(()=>{
-    console.log("ffffff")
-},2000)
+let value = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+         reject('nhhh')
+         console.log(value)
+    },3000)
+})
+console.log(value)
