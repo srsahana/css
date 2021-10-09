41dcc3b428ff3f7ffae96871e067bc33e0c3f8c3
srsahana <sahanasr24102000@gmail.com>
2021-10-09 22:17:18 +0530

    add and multiply

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-var button= document.querySelector('button')
-var body= document.querySelector('body')
-
-const callback =(ele)=>{
-    body.classList.toggle('dark')
+const a =document.querySelectorAll('input')[0]
+const b =document.querySelectorAll('input')[1]
+const add =document.querySelector('#add')
+const multiply =document.querySelector('#multiply')
+const resultbox =document.querySelector('.result')
+const sum =()=>{
+    const result=parseInt(a.value)+parseInt(b.value)
+    resultbox.innerHTML=result
+}
+const product =()=>{
+    const result=parseInt(a.value)*parseInt(b.value)
+    resultbox.innerHTML=result
 }
 
-button.addEventListener('click',callback)
+const calculate =(op)=>{
+    if (op =='add'){
+        console.log('adding')
+    } else if(op=='multiply'){
+        console.log('multiply')
+    }
+}
+add.addEventListener('click',sum)
+multiply.addEventListener('click',product)
+
+
+add.addEventListener('click',()=>{
+    return calculate('add')
+})
+multiply.addEventListener('click',()=>{
+    return calculate('multiply')
+})
