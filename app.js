24dbfcddb95f09bae14a09f577ac528b7a7ece97
srsahana <sahanasr24102000@gmail.com>
2021-10-10 13:05:11 +0530

    filter and array methods

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var arr =[1,2,3,4,5,6,7]
-arr.filter((ele, index)=>{
-    console.log(ele, index)
-
+var newa=arr.filter((ele, index)=>{
+    if(ele%2==0) return true
 })
 // var newa =[]
 // for(var i=0;i<arr.length;i++){
 //     if(arr[i]%2==0) newa.push(arr[i])
 // }
-// console.log(newa)
+console.log(newa)
+
