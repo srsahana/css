1844bba9c9fa70b05849fdd6cb4b83ffe4de6a40
srsahana <sahanasr24102000@gmail.com>
2021-10-10 12:53:51 +0530

    filter

--- app.js
+++ app.js
@@ -1,49 +1,10 @@
-const a =document.querySelectorAll('input')[0]
-const b =document.querySelectorAll('input')[1]
-const add =document.querySelector('#add')
-const multiply =document.querySelector('#multiply')
-const subtract =document.querySelector('#subtract')
-const resultbox =document.querySelector('.result')
+var arr =[1,2,3,4,5,6,7]
+arr.filter((ele, index)=>{
+    console.log(ele, index)
 
-const sum =()=>{
-    const result=parseInt(a.value)+parseInt(b.value)
-    resultbox.innerHTML=result
-}
-const product =()=>{
-    const result=parseInt(a.value)*parseInt(b.value)
-    resultbox.innerHTML=result
-}
-const sub =()=>{
-    const result=parseInt(a.value)-parseInt(b.value)
-    resultbox.innerHTML=result
-}
-
-const calculate =(op)=>{
-    switch(op){
-        case "add":
-            resultbox.innerHTML =parseInt(a.value)+parseInt(b.value)
-            break
-        case "multiply":
-            resultbox.innerHTML =parseInt(a.value)*parseInt(b.value)
-            break
-        case "subtract":
-            resultbox.innerHTML =parseInt(a.value)-parseInt(b.value)
-            break
-        default:
-            resultbox.innerHTML ="Not valid"
-    }
-}
-add.addEventListener('click',sum)
-multiply.addEventListener('click',product)
-subtract.addEventListener('click',sub)
-
-
-add.addEventListener('click',()=>{
-    return calculate('add')
 })
-subtract.addEventListener('click',()=>{
-    return calculate('subtract')
-})
-multiply.addEventListener('click',()=>{
-    return calculate('multiply')
-})
+// var newa =[]
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]%2==0) newa.push(arr[i])
+// }
+// console.log(newa)
