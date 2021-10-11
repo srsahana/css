25d6e42a9414bcee5948e1d1fda0438aa942621c
srsahana <sahanasr24102000@gmail.com>
2021-10-11 18:18:50 +0530

    json stringify

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const { add,subtract,multiply, divide}=require('./utils.js')
-add()
-subtract()
-multiply()
-divide()
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
+
+// div.innerHTML=localStorage.getItem('Inputvalue')
+
+// button.addEventListener('click', (req, res) => {
+//     localStorage.setItem('Inputvalue',[1,2,3,4,5])
+//     div.innerHTML = input.value
+// })
+
+const arr=[1,2,3,4]
+
+localStorage.setItem('val', JSON.stringify(arr))
+console.log((localStorage.getItem('val')))
+
