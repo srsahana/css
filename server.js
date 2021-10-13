f5e07dc43ae2bc7132b19ef9de00336709d41e61
srsahana <sahanasr24102000@gmail.com>
2021-10-13 22:14:27 +0530

    http request

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("fffff")
+const http = require('http')
+
+const server = http.createServer((request,response)=>{
+    //request.url
+    console.log(request.url)
+    console.log(request.method)
+})
+
+console.log(server)
+server.listen(3000,()=>{
+    console.log(`Server lisening at PORT: ${3000}`)
+})
