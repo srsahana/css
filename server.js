8fbeff5f2bdaacfb781dcf60d786c45a81608388
srsahana <sahanasr24102000@gmail.com>
2021-10-14 18:34:18 +0530

    port get

--- server.js
+++ server.js
@@ -1,36 +1,20 @@
-// const http = require('http')
-// const fs =require('fs')
-// const path =require('path')
+const express=require('express')
+const PORT=3000
+const app =express()
 
-// const server = http.createServer((request,response)=>{
-//     //request.url
-//     const {url}=request
-//     if (url=='/login'){
-//         response.write(`<h1>LOGIN</h1>`)
-//         response.end()
-//     }
-//     if (url=='/signup'){
-//         response.write(`<h1>SIGNUP</h1>`)
-//         response.end()
-//     }
-// })
+const verify = (req,res,next)=>{
+    if(req.headers['user-agent']==="PostmanRuntime/7.28.4") next()
+    else res.send("BLOCKED")
 
-// console.log(server)
-// server.listen(3000,()=>{
-//     console.log(`Server lisening at PORT: ${3000}`)
-// })
+}
 
-const data=[{ name:"Abc"},{ name:"Ggg"},{ name:"Jjj"}]
-
-const express = require('express')
-const app = express()
-
-app.get('/getNames',(req,res)=>{
-    res.send(data)
+app.get('/',verify,(req,res)=>{
+    res.send("VERIFIED")
+    
 })
 
 
 
-app.listen(3000,()=>{
-    console.log("server lisening at port 3000")
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`)
 })
