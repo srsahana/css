45a1d2305a1ccb9be699f52959b0d221e69fd85c
srsahana <sahanasr24102000@gmail.com>
2021-10-14 15:36:07 +0530

    express

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const http = require('http')
-const fs =require('fs')
-const path =require('path')
+// const http = require('http')
+// const fs =require('fs')
+// const path =require('path')
 
-const server = http.createServer((request,response)=>{
-    //request.url
-    //console.log(request.url)
-    //const path =request.url
-    const markup =fs.readFileSync(path.resolve('./index.html'))
-    response.write(markup)
-    response.end()
+// const server = http.createServer((request,response)=>{
+//     //request.url
+//     const {url}=request
+//     if (url=='/login'){
+//         response.write(`<h1>LOGIN</h1>`)
+//         response.end()
+//     }
+//     if (url=='/signup'){
+//         response.write(`<h1>SIGNUP</h1>`)
+//         response.end()
+//     }
+// })
+
+// console.log(server)
+// server.listen(3000,()=>{
+//     console.log(`Server lisening at PORT: ${3000}`)
+// })
+
+
+const express = require('express')
+const app = express()
+
+app.get('/login',(req,res)=>{
+    res.send('This is LOGIN')
+})
+app.get('/signup',(req,res)=>{
+    res.send('This is SIGNUP')
 })
 
-console.log(server)
-server.listen(3000,()=>{
-    console.log(`Server lisening at PORT: ${3000}`)
+
+app.listen(3000,()=>{
+    console.log("server lisening at port 3000")
 })
