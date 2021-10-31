024b6915d5f07e80214856b2f2566aebed564e17
srsahana <sahanasr24102000@gmail.com>
2021-10-31 22:43:12 +0530

    status

--- database/schema/routes/categoryRoutes.js
+++ database/schema/routes/categoryRoutes.js
@@ -1,17 +1,16 @@
 const database=require('../database/db')
 const express=require('express')
-const { request, response } = require('express')
 const router =express.Router()
 
-router.get('/all',()=>{
+router.get('/all',(req,res)=>{
     try {
-        response.json({
+        res.status(200).json({
             categories:database.categories,
             message:"Successfully fetched categories",
             status:"SUCCESS"
         })
     } catch (error) {
-        response.json({
+        res.status(200).json({
             categories:[],
             message:error.message,
             status:"FAILED"
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app =express()
 app.use(express.json())
 
 
-
 app.listen(3001,()=>{
     console.log("Listening at port:3001")
 })
