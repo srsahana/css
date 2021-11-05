2203bef99ec1f0299d7c8fc4f14dc243cbc179e4
srsahana <sahanasr24102000@gmail.com>
2021-11-05 17:54:24 +0530

    adding element using react

---
+++ app.js
@@ -0,0 +1,8 @@
+const container= document.getElementById("app")
+
+const content=React.createElement('div',{},
+React.createElement('h1',{},'This is bluhh'),
+React.createElement('p',{},'hiiii bluhh')
+)
+
+ReactDOM.render(content, container)
